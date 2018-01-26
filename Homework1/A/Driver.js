d28a651ef2b156e763c48471fd61d517a07e76f6
bgingell <b.gingell@gmail.com>
2018-01-25 23:30:53 -0800

    added movement to phong lights

--- Homework1/A/Driver.js
+++ Homework1/A/Driver.js
@@ -10,7 +10,7 @@ function main(){
     let camera, scene, renderer;
 
     camera = new THREE.PerspectiveCamera(50.0,
-                    window.innerWidth/window.innerHeight, 0.1, 10);
+                    window.innerWidth/window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
 
     scene = new THREE.Scene();
@@ -21,11 +21,18 @@ function main(){
     // a light is three vec3 in the order
     // position, diffuse, specular
     let lights = [];
+    let lightdirs = [1, 1, 1];
     let phong_material = init_lights(lights);
+
     let geo_one = new THREE.SphereGeometry(1, 64, 64);
-    let mat2 = new THREE.MeshPhongMaterial();
     mesh1 = new THREE.Mesh(geo_one, phong_material);
+    mesh1.position.x -= 3;
     scene.add(mesh1);
+
+    let geo_two = new THREE.RingGeometry()
+    mesh2 = new THREE.Mesh(geo_two);
+    scene.add(mesh2);
+    
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0x999999);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -33,6 +40,8 @@ function main(){
 
     function animate() {
         requestAnimationFrame( animate );
+        move_lights(lights, lightdirs);
+        mesh1.position.z -= 0.025;
         renderer.render( scene, camera );
     }
     animate();
@@ -40,7 +49,30 @@ function main(){
 
 
 }
-
+function move_lights(lights, lightdirs){
+    console.log(lightdirs);
+    if(lightdirs[0] == 1){
+        lights[0][0].x += 0.25;
+        if(lights[0][0].x >= 10) lightdirs[0] = 0;
+    } else {
+        lights[0][0].x -= 0.25;
+        if(lights[0][0].x <= -10) lightdirs[0] = 1;
+    }
+    if(lightdirs[1] == 1){
+        lights[1][0].y += 0.25;
+        if(lights[1][0].y >= 10) lightdirs[1] = 0;
+    } else {
+        lights[1][0].y -= 0.25;
+        if(lights[1][0].y <= -10) lightdirs[1] = 1;
+    }
+    if(lightdirs[2] == 1){
+        lights[2][0].z += 0.25;
+        if(lights[2][0].z>= 10) lightdirs[2] = 0;
+    } else {
+        lights[2][0].z -= 0.25;
+        if(lights[2][0].z <= -10) lightdirs[2] = 1;
+    }
+}
 function init_lights( lights){
     light1 = [];
     let l1_pos = new THREE.Vector3(0.0, 10.0, 0.0);
