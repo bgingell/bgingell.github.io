4b4b360c69b1661b12ec37f8d7282b05dc541df6
bgingell <b.gingell@gmail.com>
2018-02-28 21:49:46 -0800

    update to add spinny red donut

--- Homework3/B/Driver.js
+++ Homework3/B/Driver.js
@@ -11,8 +11,8 @@ function main(){
     let container = document.getElementById('container');
     let camera, scene, renderer;
 
-    camera = new THREE.PerspectiveCamera(50.0,
-                    window.innerWidth/window.innerHeight, .01, 2000);
+    camera = new THREE.PerspectiveCamera(45.0,
+                    window.innerWidth/window.innerHeight, .1, 1000);
     camera.position.z = 5;
 
     scene = new THREE.Scene();
@@ -24,11 +24,17 @@ function main(){
     renderer.setClearColor(0xAAAAAA);
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
-
-
+    let light_pos = new THREE.Vector3(10.0, 10.0, 10.0);
+    let light_diff = new THREE.Vector3(1.0,1.0,0.0);
+    let light_spec = new THREE.Vector3(1.0,1.0,1.0);
+    let rote = 1.0;
     let plane = new THREE.PlaneBufferGeometry( window.innerWidth, window.innerHeight )
     let uniforms =  {
 		u_resolution: { type: "v2", value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+        light_pos: {type: "v3", value: light_pos },
+        light_diff: {type: "v3", value: light_diff },
+        light_spec: {type: "v3", value: light_spec },
+        rote: {type:"f", value: rote}
 	};
 	let material = new THREE.RawShaderMaterial( {
 		uniforms: uniforms,
@@ -39,6 +45,8 @@ function main(){
 
     function animate(){
         requestAnimationFrame( animate );
+        rote = rote + 0.2;
+        scene.children[0].material.uniforms.rote.value = rote;
         renderer.render(scene, camera);
     }
     animate();
